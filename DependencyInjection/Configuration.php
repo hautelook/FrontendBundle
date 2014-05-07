30cacd5253aad18346ef4d05c4f7226dbc8fc816
Baldur Rensch <brensch@gmail.com>
2014-05-07 14:52:03 -0700

    Initial Commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hautelook\FrontendBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('hautelook_frontend');
+
+        return $treeBuilder;
+    }
+}
